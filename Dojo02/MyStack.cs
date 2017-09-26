a852466762183f8c68c974f83a981ed1cf2f2058
Yevgen Kolesnyk <wi16b111@technikum-wien.at>
2017-09-26 13:40:41 +0200

    Dojo02 completed. All dojos are done in one solution

---
+++ Dojo02/MyStack.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo02
+{
+
+    class MyStack<T>
+    {
+        private MyContainer<T> stack = new MyContainer<T>();
+        
+        public void Push(T item)
+        {
+            if (item == null)
+            {
+                Console.WriteLine("Cannot push null item");
+                return;
+            }
+            stack.container[stack.next++] = item;
+            stack.Resize();
+        }
+        
+        public T Pop()
+        {
+            if (stack.next == 0)
+            {
+                Console.WriteLine("Stack is empty");
+                return default(T);
+            }
+            T item = stack.container[--stack.next];
+            stack.Resize();
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (stack.next == 0)
+            {
+                Console.WriteLine("Stack is empty");
+                return default(T);
+            }
+            return stack.container[stack.next - 1];
+        }
+
+
+        // Internal simulation of ArrayList
+
+        private class MyContainer<T>
+        {
+            public T[] container;
+            public int next = 0;
+
+            public MyContainer() {
+
+                this.container = new T[1];
+                this.next = 0;
+            }
+
+            public void Resize()
+            {
+                if (next >= container.Length)
+                {
+                    T[] tmp = new T[next*2];
+                    for (int i=0; i<next; i++)
+                    {
+                        tmp[i] = container[i];
+                    }
+                    container = tmp;
+                }
+
+                if (next <= container.Length / 2)
+                {
+                    T[] tmp = new T[next+1];
+                    for (int i = 0; i < next; i++)
+                    {
+                        tmp[i] = container[i];
+                    }
+                    container = tmp;
+                }
+            }
+        }
+    }
+}
---
+++ Dojo02/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo02
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("\n--------------------\nTesting with integers:\n--------------------\n");
+            TestInt();
+            Console.WriteLine("\n--------------------\nTesting with strings:\n--------------------\n");
+            TestString();
+            Console.WriteLine("\n--------------------\nTesting with objects:\n--------------------\n");
+            TestObject();
+            Console.WriteLine("\n--------------------\nTest completed\n--------------------\n");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+
+            void TestInt()
+            {
+                MyStack<int> stack = new MyStack<int>();
+
+                stack.Pop();                            // special case: empty stack
+                stack.Peek();                           // special case: empty stack
+                stack.Push(1);                          // filling stack
+                stack.Push(2);                          // filling stack
+                stack.Push(1);                          // filling stack
+                Console.WriteLine(stack.Peek());        // checking current value
+                Console.WriteLine(stack.Pop());         // removing value
+                Console.WriteLine(stack.Peek());        // checking current value
+                Console.WriteLine(stack.Pop());         // removing value
+                Console.WriteLine(stack.Peek());        // checking current value
+                Console.WriteLine(stack.Pop());         // removing value
+                stack.Pop();                            // special case: empty stack
+                stack.Peek();                           // special case: empty stack
+            }
+
+            void TestString()
+            {
+                MyStack<string> stack = new MyStack<string>();
+
+                stack.Pop();                            // special case: empty stack
+                stack.Peek();                           // special case: empty stack
+                stack.Push(null);                       // special case: pushing null value
+                stack.Push("one");                      // filling stack
+                stack.Push("two");                      // filling stack
+                stack.Push("three");                    // filling stack
+                Console.WriteLine(stack.Peek());        // checking current value
+                Console.WriteLine(stack.Pop());         // removing value
+                Console.WriteLine(stack.Peek());        // checking current value
+                Console.WriteLine(stack.Pop());         // removing value
+                Console.WriteLine(stack.Peek());        // checking current value
+                Console.WriteLine(stack.Pop());         // removing value
+                stack.Pop();                            // special case: empty stack
+                stack.Peek();                           // special case: empty stack
+            }
+
+            void TestObject()
+            {
+
+                TestObject one = new TestObject("object one");
+                TestObject two = new TestObject("object two");
+                TestObject three = new TestObject("object three");
+
+                MyStack<Object> stack = new MyStack<Object>();
+
+                stack.Pop();                            // special case: empty stack
+                stack.Peek();                           // special case: empty stack
+                stack.Push(null);                       // special case: pushing null value
+                stack.Push(one);                        // filling stack
+                stack.Push(two);                        // filling stack
+                stack.Push(three);                      // filling stack
+                Console.WriteLine(stack.Peek());        // checking current value
+                Console.WriteLine(stack.Pop());         // removing value
+                Console.WriteLine(stack.Peek());        // checking current value
+                Console.WriteLine(stack.Pop());         // removing value
+                Console.WriteLine(stack.Peek());        // checking current value
+                Console.WriteLine(stack.Pop());         // removing value
+                stack.Pop();                            // special case: empty stack
+                stack.Peek();                           // special case: empty stack
+            }
+        }
+    }
+
+    public class TestObject
+    {
+        string value;
+
+        public TestObject(string value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return this.value;
+        }
+    }
+
+}
